e21e857d8d33aa96d8157d0c34bd6f427a24e707
Evan Pavone <evanrpavone@gmail.com>
2020-07-24 14:16:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,16 +9,24 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
-  }
-  return array;
-}
+ var facts = [
+   "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+   "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+ ];
+
+ function johnLennonFacts(arr) {
+   var result= []; 
+   var i = 0; 
+   while(i < arr.length) {
+    result.push(arr[i] + '!!!')
+    i++; 
+   }
+   return result; 
+ }
+
+johnLennonFacts(facts); 
 
 function iLoveTheBeatles(number) {
   var array = [];
